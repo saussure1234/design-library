915a5a6389f9b61d9e43668dccca2c3b6e1cff48
So Hayashi <sohayashi@Sos-MacBook-Pro.local>
2026-09-09 01:18:52 +0900

    LP版管理を更新 (2026-09-09)

--- .serve_docs.py
+++
@@ -1,4 +0,0 @@
-import http.server, os, socketserver
-os.chdir("/Users/sohayashi/design-library/docs")
-socketserver.TCPServer.allow_reuse_address = True
-with socketserver.TCPServer(("127.0.0.1",8791), http.server.SimpleHTTPRequestHandler) as h: h.serve_forever()
